75c6a4b06cbc73ed3f9d1b857ef3502a978bcd06
maccwinter <matthew.winter1@louisiana.edu>
2019-11-18 23:16:17 -0600

    woah

--- RTest3_Mac.R
+++ RTest3_Mac.R
@@ -19,27 +19,29 @@ head(d)
 #Section 2 ---- 
 #Question 3 ---- 
 dir.create("Figures")
-#Question 4 ---- 
+#Question 4 a - e ---- 
 
 u <- d1[d1$tow == 'und',]
-thyme <- u$dateTime
-range(thyme)
 
 ddply(.data = u, .variables = c("transect.id"), function(x){
   
   id <- unique(x$transect.id)
   
-  p.plot <- ggplot(data = x, aes(x = dateTime, y = -depth)) +
-    geom_point() +
-    scale_x_datetime(date_breaks = "15 min", date_labels = "%H:%M" )
-  
-    ggtitle(label = id)
+  u.plot <- ggplot(data = x, aes(x = dateTime, y = -depth)) +
+    geom_point(shape = 1, color = "#000066", size = 4.5) +
+    scale_x_datetime(date_breaks = "15 min", date_labels = "%H:%M" ) +
+    geom_smooth() + 
+    ggtitle(label = id) 
   
   ggsave(filename = paste0('Figures',id,'.png'),
-         plot = p.plot, width = 4, height = 3, units = 'in',
+         plot = u.plot, width = 20, height = 10, units = 'in',
          dpi = 300)
   
 }, .inform = T, .progress = "text")
+
+#Section 3 ----- 
+#Question 5 ----
+
 
 
 
